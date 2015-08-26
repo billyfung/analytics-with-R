34de57c3538d3a65d705cf35ec675ca9b9644732
Billy Fung <billy@billyfung.com>
2015-08-25 17:35:34 -0700

    looking at flights in the USA, jan 2015

---
+++ flightsinjan2015.R
@@ -0,0 +1,15 @@
+#let's look at some flight data from jan2015 in the usa
+jan2015flights <- read.csv("~/Downloads/jan2015flights.csv")
+summary(jan2015flights)
+#max departure delay is 1988 minutes, average 9.8 minutes
+#top 5 airports of origin are: ATL, ORD, DFW, LAX, DEN
+#top 5 destinations are: ATL, ORD, DFW, LAX, DEN
+#least busy airports are: ADK, PPG, UST, OTH, ILG
+#Adak Island (alaska), pago pago, Northeast Florida Regional, southwest oregon, new castle(philly)
+table(jan2015flights$CANCELLED)
+#percentage cancelled 
+pcancel = 11982 / (11982 + 457986)
+#let's look at only the cancelled flights
+cancelled <- subset(jan2015flights, CANCELLED == 1)
+sort(table(cancelled$ORIGIN))
+#we can see that most cancelled flight originated from ORD, LGA, EWR, BOS, DFW
